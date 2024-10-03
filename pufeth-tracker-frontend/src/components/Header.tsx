792da3ea9dfd6835bb2573f5b5dfd9198abc19ae
Tudor Maxim <maximtudor98@gmail.com>
2024-10-03 17:12:52 +0300

    [frontend] updated header

--- pufeth-tracker-frontend/src/components/Header.tsx
+++ pufeth-tracker-frontend/src/components/Header.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ReactComponent as PufferLogo } from '../assets/puffer_logo.svg';
+import { ReactComponent as PufferHeaderLeft } from '../assets/puffer_header_left.svg';
+import { ReactComponent as PufferHeaderRight } from '../assets/puffer_header_right.svg';
 import styles from './styles/header.module.css';
 
 const Header = () => (
     <div className={styles.header}>
+        <PufferHeaderLeft />
         <div className={styles.title}>
             <PufferLogo className={styles.logo}/>
             <h3 className={styles.text}>Puffer Coversion Rate Tracker</h3>
         </div>
+        <PufferHeaderRight />
     </div>
 );
 
